bdfa0238c1bda976bd08146b53c61ee4b8ef836d
AleValdez <ale.v_1988@hotmail.com>
2021-05-10 19:01:41 -0300

    Update api.js

--- server/api.js
+++ server/api.js
@@ -8,4 +8,16 @@ app.post('/refactorings/fixContentToViewport', (request, response) => {
     response.status(200).send(codeForEval)
 })
 
+//Funcion de prueba, cambiar el colo del background
+function changeBodyColor(color){
+    //console.log(color);
+    document.body.style.backgroundColor= color;
+  }
+
+app.post('/test', cors(), (req, res) => {
+    const data = req.body;
+    console.log(data.token);
+    res.send(changeBodyColor.toString()+"; changeBodyColor('#025');")
+  })
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
