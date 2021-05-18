1aa15e2733cd3fdea82d3f871e1c452ba65a5624
AleValdez <ale.v_1988@hotmail.com>
2021-05-18 14:56:07 -0300

    Update enlarge_hitbox.js

--- refactorings/enlarge_hitbox.js
+++ refactorings/enlarge_hitbox.js
@@ -4,14 +4,21 @@ let style = {
 }
 
 // toma 2 parametros, el elemento del dom y el objeto de estilos a aplicar.
-function enlargeHitbox(elemDom, elemStyle){
-    // obtengo todos elementos con el tag 'a' (links).
-    let links = elemDom.querySelectorAll('a');
-    //Recorro el arreglo aplicando el cambio de estilo a cada link.
+const enlargeHitbox = (pathDom, elemStyle) => {
+    // obtengo todos elementos del path.
+    var pathLinks = document.evaluate(pathDom, document, null, XPathResult.ANY_TYPE, null);
+
+    var node = null;
+    var links = [];
+    //por cada elemento lo guardo en un arreglo.
+    while (node = pathLinks.iterateNext()) {
+        links.push(node);
+    }
+    //Recorro el arreglo aplicando el cambio de estilo a cada elemento.
     links.forEach(link => {
         //El método Object.assign() copia todas las propiedades enumerables de uno o más objetos fuente a un objeto destino. 
         //Devuelve el objeto destino. 
-        Object.assign(link.style,elemStyle); 
+        Object.assign(link.style, elemStyle);
         // en este caso la propiedad style de los links son el objeto destino y los estilos el objeto fuente.
     });
 }
