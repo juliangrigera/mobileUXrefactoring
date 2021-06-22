c4a23adcfe8f1efcd62893c9a130c2c66302535e
marzofran <fpmarzano@gmail.com>
2021-06-22 12:51:18 -0300

    Added refactoring fetch for display

--- api.js
+++ api.js
@@ -15,34 +15,53 @@ app.post('/test', cors(), (req, res) => {
     res.send(convertFunctionToString(FUNCTIONS_REFACTORING['enlargeHitbox'], '/html/body//a', {color:'red'})) //para este ejemplo, cambia todos los links a rojo.
   })
 
+//Applies refactorings for a token + an url (will be changed)
 app.post('/refactor', cors(), async(req,res) => {
+  //Request contains a user token and an url
   const data = req.body;
-  //fetch
-  connect()
-  const refactorings = await User.aggregate([
-    { $match: { 'token': data.token }}, 
-    { $unwind: "$sites" }, 
-    { $replaceRoot: { newRoot: "$sites" }}, 
-    { $match: { 'url': data.url }}, 
-    { $unwind: "$refacs" },
-    { $replaceRoot: { newRoot: "$refacs" }}
-  ]).catch( e => {
-    return console.error(e)
-  });
-  disconnect()
+  const refactorings = await getRefactorings(data);
 
+  //Forms string with code for eval function
   var stream = '';
-
-  INITIAL_FUNCTIONS.forEach( func => {
-    stream += func.toString()
-  });
-
+  INITIAL_FUNCTIONS.forEach( func => stream += func.toString());
   for (r of refactorings){
-    console.log(r)
     for (element of r.elements) stream += convertFunctionToString(FUNCTIONS_REFACTORING[r.refName], element, r.params);
   }
   console.log(stream);
-  res.send(stream).status(200)
+
+  res.send(stream).status(200).end();
 })
+
+//Fetches refactorings to display for a token + url (will be changed)
+app.post('/refactor', cors(), async(req,res) => {ç
+  //Request contains a user token and an url
+  const data = req.body;
+  const refactorings = await getRefactorings(data);
+
+  res.send(refactorings).status(200).end();
+})
+
+//Creates a new refactoring
+app.post('/:token', cors(), async(req,res) => {
+  //Request contains an url, a refactoring name, a list of xpaths and an object with parameters
+  res.status(200).end();
+})
+
+async function getRefactorings({token,url}){
+  connect();
+  const refactorings = await User.aggregate([
+    { $match: { 'token': token }}, 
+    { $unwind: "$sites" }, 
+    { $replaceRoot: { newRoot: "$sites" }}, 
+    { $match: { 'url': url }}, 
+    { $unwind: "$refacs" },
+    { $replaceRoot: { newRoot: "$refacs" }}
+  ]).catch( e => {
+    return console.error(e);
+  });
+  disconnect();
+
+  return refactorings;
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
