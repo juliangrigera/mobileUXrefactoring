5d9bcd301ec79f0383789d7edcdad623debea03e
AleValdez <ale.v_1988@hotmail.com>
2021-07-04 23:37:26 -0300

    Update User.js

--- frontend/src/components/users/User.js
+++ frontend/src/components/users/User.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
+//import { useHistory } from 'react-router';
+
 
 function Users() {
     const [users, setUsers] = useState([]);
+    //const history = useHistory();
     useEffect(() => {
         getData().then(data => setUsers(data)).catch(e => console.log(e))
     }, [])
     const getData = async () => {
-        const response = await fetch('/users');
+        console.log(localStorage.getItem('token'));
+        const response = await fetch('/users', {
+            headers:{"authorization": localStorage.getItem('token')}
+        });
         const body = await response.json();
         console.log(body);
         if (response.status !== 200) {
