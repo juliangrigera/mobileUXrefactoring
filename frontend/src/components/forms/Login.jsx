0077608a69d0e81c3d0e2015f76c190cdea7c0f5
AleValdez <ale.v_1988@hotmail.com>
2021-07-04 23:37:21 -0300

    Create Login.jsx

---
+++ frontend/src/components/forms/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'bootstrap-4-react';
+import { useHistory } from 'react-router';
+
+const Login = () => {
+
+    const history = useHistory();
+
+    const [datos, setDatos] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleInputChange = (event) => {
+        setDatos({
+            ...datos,
+            [event.target.name]: event.target.value
+        })
+        console.log(event)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        console.log(datos.username)
+        console.log(datos.password)
+        const response = await fetch('/authenticate',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    username: datos.username,
+                    password: datos.password
+                }),
+                headers: { "Content-Type": "application/json" }
+            })
+        const body = await response.json();
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        if(body.success){
+            localStorage.setItem('token', body.token); //guardo el token en el localstorage
+            history.replace('./user')
+        }
+        console.log(body.token);
+    }
+
+
+    return (
+        <Container pt="5">
+            <Form w="25" display="block" mx="auto" onSubmit={handleSubmit}>
+                <Form.Group>
+                    <label htmlFor="username">Usuario</label>
+                    <Form.Input name="username" type="text" id="username" onChange={handleInputChange} placeholder="Ingrese su usuario" />
+                </Form.Group>
+                <Form.Group>
+                    <label htmlFor="password">Password</label>
+                    <Form.Input name="password" type="password" id="password" onChange={handleInputChange} placeholder="Password" />
+                </Form.Group>
+                <Button primary>Ingresar</Button>
+            </Form>
+        </Container>
+    )
+}
+
+export default Login;
