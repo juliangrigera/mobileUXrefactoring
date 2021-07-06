28df752b4ffe5c5656d53b9129b0a68a7f64ff66
AleValdez <ale.v_1988@hotmail.com>
2021-07-06 15:16:34 -0300

    Update UserRefactorings.js

--- frontend/src/components/users/UserRefactorings.js
+++ frontend/src/components/users/UserRefactorings.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from 'bootstrap-4-react';
 
-function UserRefactoring(props){
+function UserRefactoring(props) {
     const [refactorings, setRefactorings] = useState([{
         refName: "",
         elements: [String],
         params: Object
-      }]);
+    }]);
     //const history = useHistory();
     useEffect(() => {
         getData().then(data => setRefactorings(data)).catch(e => console.log(e))
     }, [])
     const getData = async () => {
         //console.log(localStorage.getItem('token'));
-        const response = await fetch('/refactorings/'+localStorage.getItem('usertoken'), {
-            headers:{"authorization": localStorage.getItem('token')}
+        const response = await fetch('/refactorings/' + localStorage.getItem('usertoken'), {
+            headers: { "authorization": localStorage.getItem('token') }
         });
         const body = await response.json();
         console.log(body);
@@ -28,28 +28,28 @@ function UserRefactoring(props){
         display: 'inline-block',
         margin: '.25rem',
         backgroundColor: '#f5f5f5'
-      }
+    }
 
     const showRefactoring = (refactorings) => {
         const listRefactorings = refactorings.map((refactoring) =>
-        <Container style={boxStyle} border>
-        <Row bg="light" p='2'>
-            <Col>{refactoring.refName}</Col>
-         </Row>
-         <Row p='2'>
-         <Col>{refactoring.elements[0]}</Col>
-         <Col>{JSON.stringify(refactoring.params)}</Col>
-       </Row>
-       </Container>
+            <Container style={boxStyle} border>
+                <Row bg="light" p='2'>
+                    <Col>{refactoring.refName}</Col>
+                </Row>
+                <Row p='2'>
+                    <Col>{refactoring.elements[0]}</Col>
+                    <Col>{JSON.stringify(refactoring.params)}</Col>
+                </Row>
+            </Container>
         )
         return listRefactorings;
     }
 
-    return(<div>{
-            refactorings[0].refName !== "" 
-                ? showRefactoring(refactorings)
-                :<p> Loading...</p>
-        }</div>)
+    return (<div>{
+        refactorings[0].refName !== ""
+            ? showRefactoring(refactorings)
+            : <p> Loading...</p>
+    }</div>)
 }
 
 export default UserRefactoring
