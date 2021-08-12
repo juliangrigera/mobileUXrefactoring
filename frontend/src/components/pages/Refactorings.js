e71988ffa9a62c6423e77629e6c40d62e822ae17
AleValdez <ale.v_1988@hotmail.com>
2021-08-12 18:28:23 -0300

    Update Refactorings.js

--- frontend/src/components/pages/Refactorings.js
+++ frontend/src/components/pages/Refactorings.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {BDiv} from 'bootstrap-4-react';
 import Code from '../utils/Code';
-import UserRefactoring from '../users/UserRefactorings';
 import Header from "./../layout/Header";
 import { Container } from 'bootstrap-4-react';
 import GenerateToken from '../forms/GenerateToken';
+import ShowVersions from '../users/versions/ShowVersions';
 
 function RefactoringPage(props) {
     return (
@@ -15,8 +15,8 @@ function RefactoringPage(props) {
                 <Code />
                 <h3>Genere un nuevo token para poner en su web</h3>
                 <GenerateToken />
-                <h3> Listado de refactorings activos para esta version</h3>
-                <UserRefactoring />
+                <h3> Versiones con los refactorings aplicados a cada una de ellas</h3>
+                <ShowVersions />
             </BDiv>
         </Container>
     )
