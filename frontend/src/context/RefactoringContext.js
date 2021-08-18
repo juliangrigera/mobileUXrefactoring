40c43e4af8c1b1753a23d44c8cdc2198ff9e6b46
AleValdez <ale.v_1988@hotmail.com>
2021-08-18 18:43:29 -0300

    Create RefactoringContext.js

---
+++ frontend/src/context/RefactoringContext.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Refactoring = React.createContext({
+    refName: '',
+    elements: [],
+    params: {},
+    versions: [] //valores por defecto
+});
+export default Refactoring;
