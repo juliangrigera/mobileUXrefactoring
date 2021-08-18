648ce14048471cfd3032b530131899754def2384
AleValdez <ale.v_1988@hotmail.com>
2021-08-18 18:43:18 -0300

    Update Nav.js

--- frontend/src/components/layout/Nav.js
+++ frontend/src/components/layout/Nav.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'bootstrap-4-react';
+import { Navbar, Nav, Dropdown } from 'bootstrap-4-react';
 
 export default class NavPrimary extends Component {
-  render() {
-      return (
-          <Navbar expand="lg" dark bg="dark" mb="3">
-              <Navbar.Brand href="#">RE-FAC</Navbar.Brand>
-              <Navbar.Toggler target="#navbarColor1" />
-              <Navbar.Nav mr="auto">
-                  <Nav.ItemLink active href="/user">Home</Nav.ItemLink>
-                  <Nav.ItemLink href="/refactorings">Refactorings</Nav.ItemLink>
-              </Navbar.Nav>
-          </Navbar>
-      )
-  }
+    render() {
+        return (
+            <Navbar expand="lg" dark bg="dark" mb="3">
+                <Navbar.Brand href="#">RE-FAC</Navbar.Brand>
+                <Navbar.Toggler target="#navbarColor1" />
+                <Navbar.Nav mr="auto">
+                    <Nav.ItemLink active href="/user">Home</Nav.ItemLink>
+                    <Nav.Item dropdown>
+                        <Nav.Link dropdownToggle>Refactoring</Nav.Link>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/refactorings">Detalles</Dropdown.Item>
+                            <Dropdown.Item href="/addRefactoring">Agregar Nuevo</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Nav.Item>
+                </Navbar.Nav>
+            </Navbar>
+        )
+    }
 }
