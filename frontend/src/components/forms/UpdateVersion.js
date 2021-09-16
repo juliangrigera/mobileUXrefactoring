0bdebfa9ac435c78aa41517f06e656a5f44b3d11
Alejandro Valdez <ale.v_1988@hotmail.com>
2021-09-16 14:44:14 -0300

    update
    informs if there was success in the action carried out within the modal.

--- frontend/src/components/forms/UpdateVersion.js
+++ frontend/src/components/forms/UpdateVersion.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from 'bootstrap-4-react';
 
+
 const UpdateVersionForm = (props) => {
-    
-    const {version} = props;
-    console.log(version)
+
+    const { version } = props;
+
+    const setShow = props.setShow;
 
     const [datos, setDatos] = useState({
         name: version.name,
@@ -22,7 +24,7 @@ const UpdateVersionForm = (props) => {
     const handleSubmit = async (event) => {
         event.preventDefault()
         
-        const response = await fetch('/versions/'+localStorage.getItem('usertoken')+'/'+version.tag,
+        const response = await fetch('/versions/' + localStorage.getItem('usertoken') + '/' + version.tag,
             {
                 method: 'PUT',
                 body: JSON.stringify({
@@ -31,40 +33,45 @@ const UpdateVersionForm = (props) => {
                     tag: datos.tag,
                     id: version._id
                 }),
-                headers: { "Content-Type": "application/json", 
-                            "authorization": localStorage.getItem('token') }
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": localStorage.getItem('token')
+                }
             })
 
         const body = await response.json();
-        if (!body.success && body.success!=='undefined') {
-            if(body.status===403){
+        if (!body.success && body.success !== 'undefined') {
+            if (body.status === 403) {
                 localStorage.removeItem('token');
                 localStorage.removeItem('usertoken');
             }
             alert(body.mensaje)
+        } else {
+            setShow({
+                isOpen: false
+            })
         }
-
     }
 
-    return(
+    return (
         <Container >
-        <Form w="100" display="block" mx="auto" onSubmit={handleSubmit}>
-            <Form.Group>
-                <label htmlFor="name">Nombre</label>
-                <Form.Input required name="name" type="text" id="name" onChange={handleInputChange} value={datos.name} />
-            </Form.Group>
-            <Form.Group>
-                <label htmlFor="description">Descripción</label>
-                <Form.Textarea name="description"  id="description" onChange={handleInputChange} value={datos.description} rows="5"></Form.Textarea>
-            </Form.Group>
-            <Form.Group>
-                <label htmlFor="tag">Tag de version</label>
-                <Form.Input required name="tag" type="text" id="tag" onChange={handleInputChange} value={datos.tag}/>
-            </Form.Group>
-            <Button primary w="100">Modificar versión</Button>
-        </Form>
-    </Container>
+            <Form w="100" display="block" mx="auto" onSubmit={handleSubmit}>
+                <Form.Group>
+                    <label htmlFor="name">Nombre</label>
+                    <Form.Input required name="name" type="text" id="name" onChange={handleInputChange} value={datos.name} />
+                </Form.Group>
+                <Form.Group>
+                    <label htmlFor="description">Descripción</label>
+                    <Form.Textarea name="description" id="description" onChange={handleInputChange} value={datos.description} rows="5"></Form.Textarea>
+                </Form.Group>
+                <Form.Group>
+                    <label htmlFor="tag">Tag de version</label>
+                    <Form.Input required name="tag" type="text" id="tag" onChange={handleInputChange} value={datos.tag} />
+                </Form.Group>
+                <Button primary w="100">Modificar versión</Button>
+            </Form>
+        </Container>
     )
-    
+
 }
 export default UpdateVersionForm;
--- frontend/src/components/modals/ModalUpdateVersion.js
+++ frontend/src/components/modals/ModalUpdateVersion.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Modal } from 'bootstrap-4-react';
 import UpdateVersionForm from '../forms/UpdateVersion';
+import { closeModal } from '../utils/FunctionsUtils';
 
 const ModalUpdateVersion = (props) => {
 
     const { version } =  props;
+    const [show, setShow] =  useState({
+        isOpen: true
+    });
 
+    const setUpdate = props.setUpdate;
+    
+    useEffect(() => {
+        if(show.isOpen === false ){
+            closeModal("modify-" + version.tag);
+            setUpdate(true);// avisa al padre q actualizo
+            setShow({
+                isOpen : true
+            })
+        }
+    }, [show])
     return (
         <Modal id={"modify-" + version.tag} fade>
             <Modal.Dialog centered>
@@ -17,7 +32,7 @@ const ModalUpdateVersion = (props) => {
                         </Modal.Close>
                     </Modal.Header>
                     <Modal.Body>
-                        <UpdateVersionForm version={version} />
+                        <UpdateVersionForm version={version} show={show.isOpen} setShow={setShow}/>
                     </Modal.Body>
                 </Modal.Content>
             </Modal.Dialog>
