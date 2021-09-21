7a742a7558038cbf4834b1d565f0df035a04e56b
Alejandro Valdez <ale.v_1988@hotmail.com>
2021-09-21 18:42:43 -0300

    Update EnlargeHitboxParams.js
    a checkbox is added to mark if it applies to checkbox / radiobutton

--- frontend/src/components/forms/params/EnlargeHitboxParams.js
+++ frontend/src/components/forms/params/EnlargeHitboxParams.js
@@ -1,23 +1,37 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Form } from 'bootstrap-4-react';
 
 const EnlargeHitboxParams = (props) => {
-    const [size, setSize] = useState(props.value !== undefined ? props.value : {padding: 0})
+    const [size, setSize] = useState(props.value !== undefined ? props.value : { padding: 0 })
+    const [check, setCheck] = useState(false);
+
+    const handleCheckBox = (event) => {
+        setCheck(event.target.checked);
+        console.log(check +' '+ event.target.checked)
+    }
 
     const handleInputParams = (event) => {
-        //let value = event.target.value
-        setSize({"padding":event.target.value});
         const func = props.bind;
-        func({"padding":event.target.value+"em"});
+        if (check) {
+            setSize({ "transform": "scale("+event.target.value+")" });
+            func({ "transform": "scale("+event.target.value+")" });
+        } else {
+            setSize({ "padding": event.target.value });
+            func({ "padding": event.target.value + "em" });
+        }
     }
     console.log(size);
-        return (
-            <Form.Group>
-                <label htmlFor="porcentage">Tamaño del Hitbox</label>
-                <Form.CustomRange id="porcentage" min="0" max="5" step="0.5" onChange={handleInputParams} value={size.padding}/>
-                <p>{size.padding+"em"}</p>
-            </Form.Group>
-        )
+    return (
+        <Form.Group>
+            <Form.Check>
+                <Form.CheckInput type="checkbox" id="check" onChange={handleCheckBox} />
+                <Form.CheckLabel htmlFor="check">Marcar si se desea aplicar sobre CheckBox/RadioButton</Form.CheckLabel>
+            </Form.Check>
+            <label htmlFor="porcentage">Tamaño del Hitbox</label>
+            <Form.CustomRange id="porcentage" min="0" max="5" step="0.5" onChange={handleInputParams} value={size.padding} />
+            <p>{(check) ? size.transform : size.padding + "em"}</p>
+        </Form.Group>
+    )
 }
 
 export default EnlargeHitboxParams;
